2bf7c0296f92fc5ea21babf515f005442fed4fb2
Juri Baumberger <juri.baumberger@karlstorz.com>
2020-07-15 10:40:05 -0700

    -Add IOCTL support on RDMA device node to pin CUDA memory -Change Makefile to find Nvidia headers and link with Nvidia module

--- XDMA/linux-kernel/xdma/cdev_rdma.c
+++ XDMA/linux-kernel/xdma/cdev_rdma.c
@@ -1,7 +1,79 @@
 
 #include <linux/ioctl.h>
+#include <linux/types.h>
+#include <nv-p2p.h>
 #include "xdma_cdev.h"
 #include "cdev_ctrl.h"
+#include "cdev_rdma.h"
+
+#define GPU_PAGE_SHIFT	16
+#define GPU_PAGE_SIZE	(((u64)1) << GPU_PAGE_SHIFT)
+#define GPU_PAGE_OFFSET	(GPU_PAGE_SIZE - 1)
+#define GPU_PAGE_MASK	(~GPU_PAGE_OFFSET)
+
+struct rdma_cuda_surface {
+	struct xdma_cdev		*xcdev;
+	u64				va;
+	u64				offset;
+	u64				len;
+	int				handle;
+	struct nvidia_p2p_page_table	*page_table;
+};
+
+static void rdma_p2p_free_callback(void *data)
+{
+	struct rdma_cuda_surface *cusurf = data;
+	nvidia_p2p_free_page_table(cusurf->page_table);
+	kfree(cusurf);
+}
+
+static int rdma_ioctl_pin_cuda(struct xdma_cdev *xcdev, unsigned long arg)
+{
+	printk(KERN_ALERT "Moo RDMA IOCTL\n");
+	void __user *argp = (void __user *)arg;
+	struct rdma_pin_cuda pin_params;
+	struct rdma_cuda_surface *cusurf;
+	u64 aligned_len;
+	int ret;
+	
+	if (copy_from_user(&pin_params, argp, sizeof(pin_params)))
+		return -EFAULT;
+	
+	cusurf = kzalloc(sizeof(*cusurf), GFP_KERNEL);
+	if (!cusurf)
+		return -ENOMEM;
+		
+	cusurf->xcdev = xcdev;
+	cusurf->va = pin_params.va & GPU_PAGE_MASK;
+	cusurf->offset = pin_params.va & GPU_PAGE_OFFSET;
+	cusurf->len = pin_params.size;	
+	aligned_len = (cusurf->offset + cusurf->len + GPU_PAGE_SIZE - 1) & GPU_PAGE_MASK;
+	
+	ret = nvidia_p2p_get_pages(0, 0, cusurf->va, aligned_len, &cusurf->page_table, rdma_p2p_free_callback, cusurf);
+	if (ret < 0) {
+		kfree(cusurf);
+		return ret;
+	}
+	
+	return 0;
+}
+
+static long rdma_fops_unlocked_ioctl(struct file *filep, unsigned int cmd, unsigned long arg)
+{
+	struct xdma_cdev *xcdev = (struct xdma_cdev *)filep->private_data;
+	int result;
+
+	result = xcdev_check(__func__, xcdev, 0);
+	if (result < 0)
+		return result;
+		
+	switch (cmd) {
+	case RDMA_IOC_PIN:
+		return rdma_ioctl_pin_cuda(xcdev, arg);
+	default:
+		return -EINVAL;
+	}
+}
 
 /*
  * character device file operations for control bus (through control bridge)
@@ -10,6 +82,7 @@ static const struct file_operations ctrl_fops = {
 	.owner = THIS_MODULE,
 	.open = char_open,
 	.release = char_close,
+	.unlocked_ioctl = rdma_fops_unlocked_ioctl,
 };
 
 void cdev_rdma_init(struct xdma_cdev *xcdev)
---
+++ XDMA/linux-kernel/xdma/cdev_rdma.h
@@ -0,0 +1,33 @@
+struct rdma_pin_cuda {
+	/* In */
+	__u64 va;
+	__u64 size;
+	/* Out */
+	__u32 handle;
+};
+
+struct rdma_unpin_cuda {
+	/* In */
+	__u32 handle;
+};
+
+struct rdma_h2c2h_dma {
+	/* In */
+	/* Malloc: Pointer, CUDA: Handle from RDMA_IOC_PIN */
+	__u64 src;
+	/* Malloc: Pointer, CUDA: Handle from RDMA_IOC_PIN */
+	__u64 dst;
+	__u64 len;
+	__u64 flags;
+	/* Out */
+	__u64 dma_time_ns;
+};
+#define RDMA_H2C2H_DMA_FLAG_SRC_IS_CUDA (1 << 0)
+#define RDMA_H2C2H_DMA_FLAG_DST_IS_CUDA (1 << 1)
+
+enum RDMA_IOC_TYPES {
+	RDMA_IOC_PIN,
+	RDMA_IOC_UNPIN,
+	RDMA_IOC_H2C2H_DMA,
+	RDMA_IOC_MAX
+};
